afdca914d5f54e5da1ecb5d8d7c16467e72f1f0a
goleng10 <133832778+goleng10@users.noreply.github.com>
2023-05-17 14:29:47 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Aspire 3\\Downloads\\flutter_windows_3.7.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\tesb" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Aspire 3\\Downloads\\flutter_windows_3.7.3-stable\\flutter"
+  "PROJECT_DIR=D:\\tesb"
+  "FLUTTER_ROOT=C:\\Users\\Aspire 3\\Downloads\\flutter_windows_3.7.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\tesb\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\tesb"
+  "FLUTTER_TARGET=D:\\tesb\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\tesb\\.dart_tool\\package_config.json"
+)
